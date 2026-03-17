946e050b953e8626f8f754353e8a40f1773efa57
Adil <muratowadil@gmail.com>
2026-03-17 19:07:11 +0600

    fix and update first migration

--- app/core/models/request.py
+++ app/core/models/request.py
@@ -1,9 +1,13 @@
-from sqlalchemy import Text, Boolean
+from sqlalchemy import String, Text, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
 
 class Request(Base):
+    model_name: Mapped[str] = mapped_column(
+        String,
+        default=""
+    )
     text: Mapped[str] = mapped_column(
         Text, 
         default=""
